7ec7bae1b61d05a73c987802d59be28d3a8625eb
akkapakasaikiran <180050005@iitb.ac.in>
2020-01-02 12:18:19 +0530

    2 Jan 2019

---
+++ Codeforces/599b1.cpp
@@ -0,0 +1,99 @@
+#include<iostream>
+#include<vector>
+#include<list>
+#include<cmath>
+#include<set>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+#include<stack>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+#define ll long long
+#define st string
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vector<int> >
+#define vb vector<bool>
+#define vii vector<int>::iterator
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define pii pair<int,int>
+#define mp make_pair
+#define fi first
+#define se second
+
+//////////////////////////////////////////
+
+#define test(t) int t; cin>>t; while(t--)
+#define rep(i,n) for(int i=0;i<n;i++)
+#define rep1(i,n) for(int i=1;i<=n;i++)
+
+////////////////////////////////////////////
+
+#define tki take_input
+#define tkii take_input<int>
+template <typename T>
+void take_input(vector<T> &a, int size){
+	T tmp;
+	for(int i=0;i<size;i++){
+		cin>>tmp;
+		a.push_back(tmp);
+	}
+}
+
+#define prv print_vector
+#define prvi print_vector<int>
+template <typename T>
+void print_vector(vector<T> v){
+	for(int i=0;i<v.size();i++) cout<<v[i]<<" ";
+	cout<<endl;
+}
+
+int vmax(vi v){ //positivity assumed
+	int ans = 0;
+	rep(i,v.size()) ans=max(ans,v[i]);
+	return ans;
+}
+
+////////////////////////////////////////
+
+#define ae add_edge
+void add_edge(vector<vi> &a, int u, int v){
+	a[u].pb(v);
+}
+
+#define prg print_graph
+void print_graph(vector<vi> &a){
+	rep(i,a.size()){ cout<<i<<" : "; prvi(a[i]); }
+}
+
+/////////////////////////////////////////
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	test(t){
+		int n; cin>>n;
+		st s,t; cin>>s>>t;
+		char x,y;
+		int cnt = 0;
+		bool ok = false;
+		rep(i,n){
+			if(s[i]!=t[i]){
+				if(cnt==0){ cnt++; x=s[i]; y=t[i]; }
+				else if(cnt==1){
+					ok = (s[i]==x && t[i]==y);
+					cnt++;
+				}
+				else ok = false;
+			}
+		}
+		if(ok) cout<<"Yes"<<endl;
+		else cout<<"No"<<endl;
+	}
+}
---
+++ Codeforces/599c.cpp
@@ -0,0 +1,97 @@
+#include<iostream>
+#include<vector>
+#include<list>
+#include<cmath>
+#include<set>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+#include<stack>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+#define ll long long
+#define st string
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vector<int> >
+#define vb vector<bool>
+#define vii vector<int>::iterator
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define pii pair<int,int>
+#define mp make_pair
+#define fi first
+#define se second
+
+//////////////////////////////////////////
+
+#define test(t) int t; cin>>t; while(t--)
+#define rep(i,n) for(int i=0;i<n;i++)
+#define rep1(i,n) for(int i=1;i<=n;i++)
+
+////////////////////////////////////////////
+
+#define tki take_input
+#define tkii take_input<int>
+template <typename T>
+void take_input(vector<T> &a, int size){
+	T tmp;
+	for(int i=0;i<size;i++){
+		cin>>tmp;
+		a.push_back(tmp);
+	}
+}
+
+#define prv print_vector
+#define prvi print_vector<int>
+template <typename T>
+void print_vector(vector<T> v){
+	for(int i=0;i<v.size();i++) cout<<v[i]<<" ";
+	cout<<endl;
+}
+
+int vmax(vi v){ //positivity assumed
+	int ans = 0;
+	rep(i,v.size()) ans=max(ans,v[i]);
+	return ans;
+}
+
+////////////////////////////////////////
+
+#define ae add_edge
+void add_edge(vector<vi> &a, int u, int v){
+	a[u].pb(v);
+}
+
+#define prg print_graph
+void print_graph(vector<vi> &a){
+	rep(i,a.size()){ cout<<i<<" : "; prvi(a[i]); }
+}
+
+/////////////////////////////////////////
+
+bool isprime(ll n){
+	for(int i=2;i<=sqrt(n);i++) if(n%i==0) return false;
+	return true;
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	ll n; cin>>n;
+	vector<ll> primes;
+	ll N = n;
+
+	if(n==1){ cout<<1<<endl; return 0; }
+	for(int i=2;i<=sqrt(n);i++){
+		if(n%i==0) primes.pb(i);
+		while(n%i==0) n/=i;
+	}
+	if(n!=1) primes.pb(n);
+	if(primes.size()>1) cout<<1<<endl;
+	else cout<<primes[0]<<endl;
+}
